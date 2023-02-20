0f8c790ad2139ac50c80b111b04d01d647fa0aee
TomArnaez <tomarnaez@gmail.com>
2023-02-20 19:36:44 +0000

    Calculate normal vectors for vertices

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -10,7 +10,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/type_ptr.hpp>  
 
 #include "graphics/models/cube.hpp"
 #include "graphics/shader.h"
