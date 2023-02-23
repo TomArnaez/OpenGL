b5f8417fe204ddb2a538197b46870af81b97b57e
TomArnaez <tomarnaez@gmail.com>
2023-02-23 23:32:38 +0000

    Add image files

--- OpenGLTutorial/src/main.cpp
+++ OpenGLTutorial/src/main.cpp
@@ -1,169 +1,245 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
 
 #include <fstream>
 #include <sstream>
 #include <streambuf>
 #include <string>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-std::string loadShaderSrc(const char* filename);
+void process_input(GLFWwindow* window);
 
-int main() {
-	int success;
-	char infoLog[512];
+float mixVal = 0.5f;
 
-	glfwInit();
+int main()
+{
+    glfwInit();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // openGL version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 # ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWidnowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Could not create window." << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialise GLAD" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+    if (window == NULL) {
+        std::cout << "Could not create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	glViewport(0, 0, 800, 600);
+    glfwMakeContextCurrent(window);
 
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	/*
-		shaders
-	*/
+    glViewport(0, 0, 800, 600);
 
-	// compile vertex shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	std::string vertShaderSrc = loadShaderSrc("assets/vertex_core.glsl");
-	const GLchar* vertShader = vertShaderSrc.c_str();
-	glShaderSource(vertexShader, 1, &vertShader, NULL);
-	glCompileShader(vertexShader);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// catch error
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "Error with vertex shader comp.:" << std::endl << infoLog << std::endl;
-	}
+    /*
+    
+       shaders
 
-	// compile fragment shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	std::string fragShaderSrc = loadShaderSrc("assets/fragment_core.glsl");
-	const GLchar* fragShader = fragShaderSrc.c_str();
-	glShaderSource(fragmentShader, 1, &fragShader, NULL);
-	glCompileShader(fragmentShader);
+    */
 
-	// catch error
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "Error with frag shader comp.:" << std::endl << infoLog << std::endl;
-	}
+    Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
+    Shader shader2("assets/vertex_core.glsl", "assets/fragment_core2.glsl");
 
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+    // vertex array
+    float vertices[] = {
+        // positions            colors                  texture coordinates
+        -0.5f,  -0.5f, 0.0f,     1.0f, 1.0f, 0.5f,      0.0f, 0.0f, // bottom left
+        -0.5f,   0.5f, 0.0f,     0.5f, 1.0f, 0.75f,     0.0f, 1.0f, // top left
+         0.5f,  -0.5f, 0.0f,     0.6f, 1.0f, 0.2f,      1.0f, 0.0f, // bottom right
+         0.5f,   0.5f, 0.0f,     1.0f, 0.2f, 1.0f,      1.0f, 1.0f // top right
+    };
 
-	// catch errors
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "Linking error:" << std::endl << infoLog << std::endl;
-	}
+    unsigned int indices[] = {
+        0, 1, 2, // first triangle
+        3, 1, 2 // second triangle
+    };
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+    // VAO, VBO
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-	// vertex array
+    // bind VAO
+    glBindVertexArray(VAO);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5, -0.5f, 0.0f,
-		0.0f, 0.0f, 0.0f
-	};
+    // bind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// VAO, VBO
-	unsigned int VAO, VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+    // set attribute pointers
 
-	// bind VAO
-	glBindVertexArray(VAO);
+    // positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-	// bind VBO;
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-	// set attribute pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+    // texture coordinates
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-	while (!glfwWindowShouldClose(window)) {
-		// process input
-		processInput(window);
 
-		// render
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    // TEXTURES
 
-		// draw shapes
-		glBindVertexArray(VAO);
-		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+    unsigned int texture1, texture2;
 
-		// send new frame to window
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
 
-	glfwTerminate();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	return 0;
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // load image
+    int width, height, nChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("assets/image1.jpg", &width, &height, &nChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    data = stbi_load("assets/image2.png", &width, &height, &nChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glad_glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+            std::cout << "Failed to load texture" << std::endl;
+    }
+
+    shader.activate();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    // set up EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /*
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    shader.activate();
+    shader.setMat4("transform", trans);
+
+
+    glm::mat4 trans2 = glm::mat4(1.0f);
+    trans2 = glm::scale(trans2, glm::vec3(1.5f));
+    trans2 = glm::rotate(trans2, glm::radians(15.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    shader2.activate();
+    shader2.setMat4("transform", trans);
+    */
+
+
+    while (!glfwWindowShouldClose(window)) {
+        // process input
+        process_input(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+       
+        // draw shapes
+        glBindVertexArray(VAO);
+        shader.activate();
+        shader.setFloat("mixVal", mixVal);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        
+        //trans = glm::rotate(trans, glm::radians((float)glfwGetTime() / 100), glm::vec3(0.0f, 0.0f, 1.0f));
+        //shader.setMat4("transform", trans);
+
+        /*
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+
+        shader2.activate();
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(GLuint)));
+        */
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        // second triangle
+        //glUseProgram(shaderPrograms[1]);
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(unsigned int)));
+        glBindVertexArray(0);
+
+        // send new frame to window
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glfwTerminate();
+    return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+void process_input(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 
-std::string loadShaderSrc(const char* filename) {
-	std::ifstream file;
-	std::stringstream buf;
+    // change mix value
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        mixVal += .05f;
+        if (mixVal > 1) {
+            mixVal = 1.0f;
+        }
+    }
 
-	std::string ret = "";
-	file.open(filename);
-	if (file.is_open()) {
-		buf << file.rdbuf();
-		ret = buf.str();
-	}
-	else {
-		std::cout << "Could not open " << filename << std::endl;
-	}
-
-	file.close();
-	return ret;
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        mixVal -= .05f;
+        if (mixVal < 0) {
+            mixVal = 0.0f;
+        }
+    }
 }
---
+++ OpenGLTutorial/src/shader.h
@@ -0,0 +1,34 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader {
+public:
+	unsigned int id;
+
+	Shader(const char* vertexShaderPath, const char* fragShaderPath);
+	void activate();
+
+	// utility functions
+	std::string loadShaderSrc(const char* filepath);
+	GLuint compileShader(const char* filepath, GLenum type);
+
+	/*
+		set uniform variables
+	*/
+	void setInt(const std::string& name, int value);
+	void setMat4(const std::string& name, glm::mat4 val);
+	void setFloat(const std::string& name, float value);
+};
+
+
+#endif
