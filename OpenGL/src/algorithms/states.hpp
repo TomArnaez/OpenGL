49eea476bf5215591287c0c12c75052358db84e3
TomArnaez <tomarnaez@gmail.com>
2023-02-27 23:22:16 +0000

    Add states using bitwise operators to set states

---
+++ OpenGL/src/algorithms/states.hpp
@@ -0,0 +1,97 @@
+#ifndef STATES_HPP
+#define STATES_HPP
+
+namespace States {
+	/*
+		states = 1001101
+	*/
+	// check state
+	template<typename T>
+	bool isAcive(T* states, int target) {
+		/*
+			target = 3
+			1 = 00000001
+			1 << target = 01000000
+
+			10001101 &
+			00001000
+			--------
+			00001000 == 1 << target
+
+		*/
+		return (*states & (1 << target) == (1 << target));
+	}
+
+	// activate state
+	template<typename T>
+	void activate(T* states, int target) {
+		/*
+			target = 4
+			1 = 00000001
+			1 << target = 00010000
+
+			10001101
+			00010000 |
+			--------
+			10011101
+
+		*/
+		* states |= 1 << target;
+	}
+
+	// uniquely activate state (no others can be active)
+	template<typename T>
+	void uniquelyActivate(T* states, int target) {
+		/*
+			target = 4
+			1 = 00000001
+			1 << target = 00010000
+
+			10000000
+			00010000 |
+			--------
+			10010000
+			00010000 &
+			--------
+			00010000
+		*/
+		activate<T>(states, target); // activate start first
+		*states &= 1 << target;		// deactivate others
+	}
+
+	// deactivate states
+	template<typename T>
+	void deactivate(T* states, int target) {
+		/*
+			target = 4
+			1 = 00000001
+			~(1 << target) = 11101111 
+
+			10011101
+			11101111 &
+			--------
+			10001101
+
+		*/
+		*states &= ~(1 << target);
+	}
+
+	// toggle state
+	template<typename T>
+	void toggle(T* states, int target) {
+		/*
+			target = 4
+			1 = 00000001
+			1 << target = 00010000
+
+			10001101
+			00010000 ^
+			--------
+			10011101
+
+		*/
+		* states ^= 1 << target;
+	}
+}
+
+#endif
