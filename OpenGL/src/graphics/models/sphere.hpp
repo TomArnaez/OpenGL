9c342d5a2704964d7e4d7bfa6f50f20bba4243fa
TomArnaez <tomarnaez@gmail.com>
2023-02-27 13:38:26 +0000

    Add new sphere

--- OpenGL/src/graphics/models/sphere.hpp
+++ OpenGL/src/graphics/models/sphere.hpp
@@ -2,6 +2,7 @@
 #define SPHERE_HPP
 
 #include "../model.h"
+#include <iostream>
 
 #include "modelarray.hpp"
 
@@ -10,6 +11,7 @@ public:
 	Sphere(glm::vec3 pos = glm::vec3(0.f), glm::vec3 size = glm::vec3(1.0f))
 		: Model(pos, size, true) {
 		rb.mass = 1.0f;
+		std::cout << Model::size.x << std::endl;
 	}
 
 	void init() {
@@ -20,7 +22,7 @@ public:
 class SphereArray : public ModelArray<Sphere> {
 public:
 	void init() {
-		model = Sphere(glm::vec3(0.0f), glm::vec3(0.25f));
+		model = Sphere(glm::vec3(0.0f), glm::vec3(0.0025f));
 		ModelArray::init();
 	}
 };
